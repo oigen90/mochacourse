a6ed1323110dd4b68299cecc9c44450589ee6ac3
Eugene Babushkin <oigen90@gmail.com>
2016-12-18 20:59:51 +0200

    the kind of unit - logic separation

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var getPalette = require('./lib/getPalette');
+
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-    res.render('index', { palette: ['#cc7790', '#ff5512', '#75d709'] });
+    res.render('index', { palette: getPalette() });
 });
 
 app.listen(3000, function () {
