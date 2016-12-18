7b859c24c5ed3f189b3e1f3e6745934fe198c342
Eugene Babushkin <oigen90@gmail.com>
2016-12-18 18:04:55 +0200

    server.js typo fixes. jade added to package.json

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('view engine', 'jade');
 
-app.get('/', function (res, req) {
+app.get('/', function (req, res) {
     res.render('index', { palette: ['#cc7790', '#ff5512', '#75d709'] });
 });
 
 app.listen(3000, function () {
-    console.log('The app listening on port 3000!');
+    console.log('The app listening on port 3000...');
 });
