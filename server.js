dee59c00943a6b61b408a4213a54ebbdb3cc5c8f
Eugene Babushkin <oigen90@gmail.com>
2016-12-18 16:21:38 +0200

    Web server CL output

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.get('/', function (res, req) {
+    res.render('index', { palette: ['#cc7790', '#ff5512', '#75d709'] });
+});
+
+app.listen(3000, function () {
+    console.log('The app listening on port 3000!');
+});
