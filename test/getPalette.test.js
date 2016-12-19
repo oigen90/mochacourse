832dfde2f5337c21807bee08679070d580a0f780
Eugene Babushkin <oigen90@gmail.com>
2016-12-19 03:32:26 +0200

    before and after hooks

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -4,17 +4,38 @@
 
 var assert = require('assert');
 var getPalette = require('../lib/getPalette');
+var fs = require('fs');
+
+var configFile = process.cwd() + '/config.json';
+
+function writeConfig (config, callback) {
+    fs.writeFile(configFile, JSON.stringify(config, null, 2), callback);
+}
 
 describe('getPalette', function () {
 
-    it('should throw an error if the result is not an array', function () {
-        function fetch() {
-            return 'not array';
-        }
+    var config = {};
 
-        assert.throws(function () {
-            getPalette(fetch);
-        }, /is not an array/);
+    before(function (done) {
+        fs.readFile(configFile, function (err, contents) {
+            config = JSON.parse(contents.toString());
+            done();
+        });
+    });
+    
+    afterEach(function (done) {
+        writeConfig(config, done);
+    });
+
+    it('should throw an error if the result is not an array', function (done) {
+
+        writeConfig({palette: 'string'}, function (err) {
+
+            assert.throws(getPalette, /is not an array/);
+            done();
+
+        });
+
     });
 
     it('should return an array of 3 items by default', function () {
