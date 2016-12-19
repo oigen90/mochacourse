d0793d8de613527d0effd5a9abd3039eb5438842
Eugene Babushkin <oigen90@gmail.com>
2016-12-19 06:02:22 +0200

    chai: expect instead of node assert

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -1,7 +1,7 @@
 /**
  * Created by oigen_air on 12/18/16.
  */
-var assert = require('assert');
+var expect = require('chai').expect;
 var hex2rgb = require('../lib/hex2rgb');
 
 describe('hex2rgb', function () {
@@ -9,7 +9,7 @@ describe('hex2rgb', function () {
     it('should return an error if the value is not a hex code', function (done) {
         
         hex2rgb('blue', function (error, result) {
-            assert(error);
+            expect(error).to.exist;
 
             done();
         });
@@ -19,8 +19,9 @@ describe('hex2rgb', function () {
     it('should return a correctly converted RGB value', function (done) {
 
         hex2rgb('#fff', function (error, result) {
-            assert.strictEqual(error, null);
-            assert.deepEqual(result, [255, 255, 255]);
+
+            expect(error).to.not.exist;
+            expect(result).to.deep.equal([255, 255, 255]);
 
             done();
         });
