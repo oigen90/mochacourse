3a3f287f67b1da8cd83bd789a55d52dbe0513831
Eugene Babushkin <oigen90@gmail.com>
2016-12-19 04:48:32 +0200

    chai expect in action. fixture using

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -2,47 +2,26 @@
  * Created by oigen_air on 12/18/16.
  */
 
-var assert = require('assert');
+var expect = require('chai').expect;
 var getPalette = require('../lib/getPalette');
-var fs = require('fs');
-
-var configFile = process.cwd() + '/config.json';
-
-function writeConfig (config, callback) {
-    fs.writeFile(configFile, JSON.stringify(config, null, 2), callback);
-}
 
 describe('getPalette', function () {
 
-    var config = {};
-
-    before(function (done) {
-        fs.readFile(configFile, function (err, contents) {
-            config = JSON.parse(contents.toString());
-            done();
-        });
-    });
-    
-    afterEach(function (done) {
-        writeConfig(config, done);
-    });
-
     it('should throw an error if the result is not an array', function (done) {
 
-        writeConfig({palette: 'string'}, function (err) {
+        var notArray = function () {
+            getPalette(process.cwd() + '/test/fixtures/config-palette-non-array.json');
+        };
 
-            assert.throws(getPalette, /is not an array/);
-            done();
-
-        });
+        expect(notArray).to.throw(/is not an array/);
+        done();
 
     });
 
     it('should return an array of 3 items by default', function () {
 
         var palette = getPalette();
-        assert(Array.isArray(palette), 'did not return an array');
-        assert.equal(palette.length, 3, 'did not return 3 items');
+        expect(palette).to.be.an('array').with.length(3);
 
     })
 });
